d5cf86e2a9a5bfca2c0414a4a6599c5b55a2dabf
shohelrana2002 <rana2021tang@gmail.com>
2025-11-08 00:17:35 +0600

    Payment success add

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const Stripe = require("stripe");
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 
 // middle were
 app.use(cors());
 app.use(express.json());
-
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6zoig.mongodb.net/?appName=Cluster0`;
 
@@ -65,6 +66,25 @@ async function run() {
       const result = await parcelsCollections.findOne(query);
       res.send(result);
     });
+
+    app.post("/create-payment-intent", async (req, res) => {
+      try {
+        const { amount } = req.body;
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount * 100, // 10 USD হলে 1000 হবে
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: error.message });
+      }
+    });
+
     // deploy to comment this
     await client.connect();
     await client.db("admin").command({ ping: 1 });
